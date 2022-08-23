8d122b42199f633d8ffe514c12d941c0d67be071
Kelemen Angi <angela.kelemen.nemeth@gmail.com>
2022-08-23 15:25:00 +0200

    Clear unnecessary things from react app

--- frontend/pet-clinic/src/App.js
+++ frontend/pet-clinic/src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+
 import React, { useState, useEffect } from 'react'
+import ListAllOwners from './components/ListAllOwners'
+
 
 function App() {
 
-  const [Owners, fetchOwners] = useState([])
+  const [owners, fetchOwners] = useState([])
 
   const getData = () => {
-    console.log("Something");
     fetch('http://localhost:8080/owners')
       .then((res) => res.json())
       .then((res) => {
@@ -23,26 +23,12 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Szia Kristóf!   :)</h1>
-        <ul>
-        {Owners.map((item, i) => {
-          return <li key={i}>{item.firstName}</li>
-        })}
-      </ul>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+        
+        <h1>Header</h1>
+        <ListAllOwners owners={owners}/>
+
+  
     </div>
   );
 }
--- frontend/pet-clinic/src/components/ListAllOwners.js
+++ frontend/pet-clinic/src/components/ListAllOwners.js
@@ -3,11 +3,31 @@ import React from 'react'
 const ListAllOwners = ({owners}) => {
   return (
     <div>
-      <ul>
-        {{owners}.map((item, i) => {
-          return <li key={i}>{item.firstName}+ " " + {item.lastName}</li>
+      {/* <ul>
+        {props.owners.map((item, i) => {
+          return <li key={i}>{item.firstName} {item.lastName}</li>
         })}
-      </ul>
+      </ul> */}
+
+
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>e-mail</th>
+          <th>Some buttons</th>
+        </tr>
+        {owners.map((val, key) => {
+          return (
+            <tr key={key}>
+              <td>{val.firstName}  {val.lastName}</td>
+              <td>{val.eMail}</td>
+              <td><button><a href='https://google.com' alt='Broken Link'>This is a button</a></button></td>
+            </tr>
+          )
+        })}
+      </table>
+    
+
     </div>
   )
 }
