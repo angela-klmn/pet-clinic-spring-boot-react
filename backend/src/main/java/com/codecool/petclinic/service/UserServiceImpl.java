fc5fe4f044517f35cc680e03bc3f3e7cc4c56a20
Kelemen Angi <angela.kelemen.nemeth@gmail.com>
2022-09-13 17:32:47 +0200

    Add logger to userService

--- backend/src/main/java/com/codecool/petclinic/service/UserServiceImpl.java
+++ backend/src/main/java/com/codecool/petclinic/service/UserServiceImpl.java
@@ -5,37 +5,50 @@ import com.codecool.petclinic.model.User;
 import com.codecool.petclinic.repository.RoleRepo;
 import com.codecool.petclinic.repository.UserRepo;
 import lombok.RequiredArgsConstructor;
-
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
-
+/**
+ * @author Get Arrays (https://www.getarrays.io/)
+ * @version 1.0
+ * @since 7/10/2021
+ */
 @Service
 @RequiredArgsConstructor
 @Transactional
+@Slf4j
 public class UserServiceImpl implements UserService{
     private final UserRepo userRepo;
     private final RoleRepo roleRepo;
     private final PasswordEncoder passwordEncoder;
 
 
-
     @Override
     public User saveUser(User user) {
+        log.info("Saving new user {} to the database", user.getName());
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepo.save(user);
     }
 
     @Override
     public Role saveRole(Role role) {
+        log.info("Saving new role {} to the database", role.getName());
         return roleRepo.save(role);
     }
 
     @Override
     public void addRoleToUser(String username, String roleName) {
+        log.info("Adding role {} to user {}", roleName, username);
         User user = userRepo.findByUsername(username);
         Role role = roleRepo.findByName(roleName);
         user.getRoles().add(role);
@@ -43,11 +56,13 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public User getUser(String username) {
+        log.info("Fetching user {}", username);
         return userRepo.findByUsername(username);
     }
 
     @Override
     public List<User> getUsers() {
+        log.info("Fetching all users");
         return userRepo.findAll();
     }
 }
