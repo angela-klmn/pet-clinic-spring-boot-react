0e4f834eab0c6de5780296b1a97d287d82c78044
Kelemen Angi <angela.kelemen.nemeth@gmail.com>
2022-09-14 11:50:52 +0200

    Add addRoleToUser method in userController

--- backend/src/main/java/com/codecool/petclinic/controller/UserController.java
+++ backend/src/main/java/com/codecool/petclinic/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.codecool.petclinic.controller;
 
+import com.codecool.petclinic.model.Role;
 import com.codecool.petclinic.model.AppUser;
 import com.codecool.petclinic.service.UserService;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -27,5 +29,23 @@ public class UserController {
         return ResponseEntity.created(uri).body(userService.saveUser(user));
     }
 
+    @PostMapping("/role/save")
+    public ResponseEntity<Role>saveRole(@RequestBody Role role) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/role/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveRole(role));
+    }
 
+    @PostMapping("/role/addtouser")
+    public ResponseEntity<?>addRoleToUser(@RequestBody RoleToUserForm form) {
+        userService.addRoleToUser(form.getUsername(), form.getRoleName());
+        return ResponseEntity.ok().build();
+    }
+
+
+}
+
+@Data
+class RoleToUserForm {
+    private String username;
+    private String roleName;
 }
