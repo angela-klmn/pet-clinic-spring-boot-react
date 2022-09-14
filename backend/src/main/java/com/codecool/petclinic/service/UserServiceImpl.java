b9491192d72a9bced5516cf49ba90e77b4563428
Kelemen Angi <angela.kelemen.nemeth@gmail.com>
2022-09-14 11:49:04 +0200

    Add loadUserByUsername

--- backend/src/main/java/com/codecool/petclinic/service/UserServiceImpl.java
+++ backend/src/main/java/com/codecool/petclinic/service/UserServiceImpl.java
@@ -1,7 +1,7 @@
 package com.codecool.petclinic.service;
 
 import com.codecool.petclinic.model.Role;
-import com.codecool.petclinic.model.User;
+import com.codecool.petclinic.model.AppUser;
 import com.codecool.petclinic.repository.RoleRepo;
 import com.codecool.petclinic.repository.UserRepo;
 import lombok.RequiredArgsConstructor;
@@ -18,23 +18,33 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-/**
- * @author Get Arrays (https://www.getarrays.io/)
- * @version 1.0
- * @since 7/10/2021
- */
 @Service
 @RequiredArgsConstructor
 @Transactional
 @Slf4j
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService, UserDetailsService {
     private final UserRepo userRepo;
     private final RoleRepo roleRepo;
     private final PasswordEncoder passwordEncoder;
 
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        AppUser user = userRepo.findByUsername(username);
+        if(user == null) {
+            log.error("User not found in the database");
+            throw new UsernameNotFoundException("User not found in the database");
+        } else {
+            log.info("User found in the database: {}", username);
+            Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+            user.getRoles().forEach(role -> {
+                authorities.add(new SimpleGrantedAuthority(role.getName()));
+            });
+            return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), authorities);
+        }
+    }
 
     @Override
-    public User saveUser(User user) {
+    public AppUser saveUser(AppUser user) {
         log.info("Saving new user {} to the database", user.getName());
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepo.save(user);
@@ -49,19 +59,19 @@ public class UserServiceImpl implements UserService{
     @Override
     public void addRoleToUser(String username, String roleName) {
         log.info("Adding role {} to user {}", roleName, username);
-        User user = userRepo.findByUsername(username);
+        AppUser user = userRepo.findByUsername(username);
         Role role = roleRepo.findByName(roleName);
         user.getRoles().add(role);
     }
 
     @Override
-    public User getUser(String username) {
+    public AppUser getUser(String username) {
         log.info("Fetching user {}", username);
         return userRepo.findByUsername(username);
     }
 
     @Override
-    public List<User> getUsers() {
+    public List<AppUser> getUsers() {
         log.info("Fetching all users");
         return userRepo.findAll();
     }
