e19c87061991972be70514481807249129d7b063
Kelemen Angi <angela.kelemen.nemeth@gmail.com>
2022-09-16 15:03:25 +0200

    Add pages accessible to clients

---
+++ frontend/pet-clinic/src/components/forclients/ClientPetDetails.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ClientPetDetails = () => {
+  return (
+    <div>
+      "Here are the details of your chosen pet:"
+      <p>first detail</p>
+      <p>second detail</p>
+    </div>
+  )
+}
+
+export default ClientPetDetails
